7e45fe2c9be94fc62b59d8992a5176ae3a3e7766
saket54321 <saketmmfd1410@gmail.com>
2023-12-25 17:58:30 +0530

    setting up controller and router

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose";
+import userrouter from "./routes/user.route.js";
 const app=express();
 dotenv.config();
+app.use('/api/user',userrouter);
 mongoose.connect(process.env.MONGODB).then(()=>{
     app.listen(5000,()=>{
         console.log("app is listenning on port 5000/");
